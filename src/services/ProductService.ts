7f4e8cbd18906d05b160a68660db36dca299a02d
GabrielGirotti <girottigabriel@gmail.com>
2024-08-02 11:54:53 -0300

    checking env push

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,7 +1,23 @@
+import { DraftProductSchema } from "../types";
+import { safeParse } from "valibot";
+
 type addProductProps = {
   [k: string]: FormDataEntryValue;
 };
 
 export async function addProduct(data: addProductProps) {
-  console.log(data);
+  try {
+    const result = safeParse(DraftProductSchema, {
+      name: data.name,
+      price: +data.price,
+    });
+    if (result.success) {
+      const url = "http://localhost:4000/api/products";
+      
+    } else {
+      throw new Error("Datos no validos");
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
